d1dea7f70b3f5cc6f137fe4c01d841a5eac1e346
Zachary Lockhart <zacharyjordan82@gmail.com>
2018-04-06 17:45:36 -0400

    Finishing Problem Four

--- CommonProblems.UnitTests/ProblemFourTests.cs
+++ CommonProblems.UnitTests/ProblemFourTests.cs
@@ -7,8 +7,24 @@ namespace CommonProblems.UnitTests
     public class ProblemFourTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void LargestPossibleNumber_WhenCalled_ReturnsLargestNumberCombination()
         {
+            var expected = 95021;
+            int[] numberCombination = new int[] { 50, 2, 1, 9 };
+
+            var actual = ProblemFour.LargestPossibleNumber(numberCombination);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void LargestPossibleNumber_WhenCalledWithMoreCombinations_ReturnsLargestNumberCombination()
+        {
+            var expected = 91951501;
+            int[] numberCombination = new int[] { 50, 51, 1,91, 9 };
+
+            var actual = ProblemFour.LargestPossibleNumber(numberCombination);
+
+            Assert.AreEqual(expected, actual);
         }
     }
 }
--- CommonProblems/ProblemFour.cs
+++ CommonProblems/ProblemFour.cs
@@ -8,5 +8,27 @@ namespace CommonProblems
 {
     public static class ProblemFour
     {
+        public static int LargestPossibleNumber(int[] numberCombination)
+        {
+            //Convert the ints to alpha numeric since the sort for alpha numeric is char by char
+            string[] finalList = new string[numberCombination.Length];
+            for (int i = 0; i<  numberCombination.Length; i++)
+            {
+                finalList[i] = numberCombination[i].ToString();
+            }
+
+            //Use the sort method which defaults to ascending order
+            Array.Sort(finalList);
+            //Order by descending
+            Array.Reverse(finalList);
+
+            //convert array to string then cast it to an integer
+            string finalNumAsString = "";
+            for (int i = 0; i < finalList.Length; i++)
+            {
+                finalNumAsString += finalList[i];
+            }
+            return Convert.ToInt32(finalNumAsString);
+        }
     }
 }
