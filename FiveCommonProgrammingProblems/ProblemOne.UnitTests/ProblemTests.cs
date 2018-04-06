70446c0b6812bcc964b42c241292c54032c81f1d
Zachary Lockhart <zacharyjordan82@gmail.com>
2018-04-06 16:28:58 -0400

    Problem One solution

---
+++ FiveCommonProgrammingProblems/ProblemOne.UnitTests/ProblemTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProblemOne.UnitTests
+{
+    [TestClass]
+    public class ProblemTests
+    {
+        [TestMethod]
+        public void SumWithForLoop_WhenCalled_ShouldSumTotalNumbers()
+        {
+            var expected = 50;
+            var numbers = new int[] { 15,5,3,7,2,6,2,10};
+            var actual = Problem.SumWithForLoop(numbers);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void SumWithWhileLoop_WhenCalled_ShouldSumTotalNumbers()
+        {
+            var expected = 50;
+            var numbers = new int[] { 15, 5, 3, 7, 2, 6, 2, 10 };
+            var actual = Problem.SumWithWhileLoop(numbers);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void SumWithRecursion_WhenCalled_ShouldSumTotalNumbers()
+        {
+            var expected = 50;
+            var numbers = new int[] { 15, 5, 3, 7, 2, 6, 2, 10 };
+            var actual = Problem.SumWithRecursion(numbers,0);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ FiveCommonProgrammingProblems/ProblemOne/Problem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemOne
+{
+    public static class Problem
+    {
+        public static int SumWithForLoop(int[] numbers)
+        {
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+            return sum;
+        }
+        public static int SumWithWhileLoop(int[] numbers)
+        {
+            int sum = 0;
+            int counter = 0;
+            while(counter < numbers.Length)
+            {
+                sum += numbers[counter];
+                counter++;
+            }
+            return sum;
+        }
+        public static int SumWithRecursion(int[] numbers, int currentIndex)
+        {
+            if(currentIndex == numbers.Length-1)
+                return numbers[currentIndex];
+
+            return numbers[currentIndex] + SumWithRecursion(numbers, ++currentIndex);
+        }
+    }
+}
