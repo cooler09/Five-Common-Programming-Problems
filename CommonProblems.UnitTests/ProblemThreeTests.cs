123684f7210335d8a2fecca9d8b72ba1cba8e622
Zachary Lockhart <zacharyjordan82@gmail.com>
2018-04-06 17:19:43 -0400

    Problem Three solved with 50 instead of 100 for class simplicity

---
+++ CommonProblems.UnitTests/ProblemThreeTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CommonProblems.UnitTests
+{
+    [TestClass]
+    public class ProblemThreeTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
--- CommonProblems/ProblemThree.cs
+++ CommonProblems/ProblemThree.cs
@@ -8,5 +8,16 @@ namespace CommonProblems
 {
     public static class ProblemThree
     {
+        public static long[] GetFibonacciNums(int fibonacci)
+        {
+            long[] result = new long[fibonacci];
+            result[0] = 0;
+            result[1] = 1;
+            for (int i = 2; i < fibonacci; i++)
+            {
+                result[i] = result[i - 1] + result[i - 2];
+            }
+            return result;
+        }
     }
 }
